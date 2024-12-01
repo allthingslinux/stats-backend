b9d3196c6f6aa3e996ea0c23afea16747803002b
electron271 <66094410+electron271@users.noreply.github.com>
2024-12-01 17:06:22 -0600

    more stats

--- src/express.js
+++ src/express.js
@@ -6,6 +6,9 @@ const statsTemplate = `Statistics
 ==========
 Total users: <2>
 Total unique mentions: <3>
+Total non-anonymous users: <4>
+Total anonymous users: <5>
+Total users who opted out: <6>
 
 Endpoints
 =========
@@ -19,8 +22,11 @@ function expressMain(prisma) {
     app.get('/', async (req, res) => {
         const totalUsers = await prisma.userLookup.count();
         const totalUniqueMentions = await prisma.mention.count();
+        const totalNonAnonymousUsers = await prisma.userLookup.count({ where: { anonymous: false } });
+        const totalAnonymousUsers = await prisma.userLookup.count({ where: { anonymous: true } });
+        const totalOptOutUsers = await prisma.userLookup.count({ where: { fullOptOut: true } });
         res.set('Content-Type', 'text/plain');
-        res.send(statsTemplate.replace('<2>', totalUsers).replace('<3>', totalUniqueMentions));
+        res.send(statsTemplate.replace('<2>', totalUsers).replace('<3>', totalUniqueMentions).replace('<4>', totalNonAnonymousUsers).replace('<5>', totalAnonymousUsers).replace('<6>', totalOptOutUsers));
     });
 
     app.listen(port, () => {
