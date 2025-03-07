df71978859e9db89802fe0158e765154ddfbe5ce
electron271 <66094410+electron271@users.noreply.github.com>
2025-03-07 14:40:28 -0600

    h

--- src/express.js
+++ src/express.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const { Client } = require('discord.js');
 const app = express()
 const port = 8000
+app.use(cors())
 
 const statsTemplate = `Statistics
 ==========
